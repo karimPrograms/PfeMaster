d8e9793427fcc4038fd38146e117cc3078df5de9
karimPrograms <78750964+karimPrograms@users.noreply.github.com>
2024-03-17 14:35:35 +0100

    Add files via upload

---
+++ Server_Back/src/config/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    port : 8081,
+    db: {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'ebankingapp'
+    },
+    jwt:{
+        JWT_SECRET:"ajqmsdjfjsàçàéç_(ç_(àpuoisuqfàç_éçà_isqdufçé_'çdiufué)eoidsfe6541324564erzfd43zer",
+        JWT_EXPIRES_IN:"90d",
+        JWT_COOKIE_EXPIRES:"90"
+    }
+}
